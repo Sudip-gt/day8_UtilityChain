b23a323a81607d0558e57735354c475f8d7e3295
Sudip-gt <sudippaudel6561@gmail.com>
2025-05-19 18:29:59 +0545

    changes for git deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./main.js"],
+  content: ["./index.html", "./main.js","./dist/**/*.{js,ts}"],
   theme: {
     extend: {},
   },
